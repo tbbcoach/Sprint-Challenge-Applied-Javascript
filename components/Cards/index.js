b1bbd4ffa68fd24bf8709a502b8c67b5f09468a0
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-06-14 21:02:29 -0400

    adding cards.

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -18,3 +18,82 @@
 // </div>
 //
 // Use your function to create a card for each of the articles and add the card to the DOM.
+
+
+function card(Object) {
+
+    const card =document.createElement('div');
+    const headLine = document.createElement('div');
+    const author = document.createElement('div');
+    const imgCont = document.createElement('div');
+    const img = document.createElement('img');
+    const name = document.createElement('span');
+
+    card.classList.add('card');
+    headLine.classList.add('headline');
+    author.classList.add('author');
+    imgCont.classList.add('img-container');
+
+    //add text content here...
+
+    headLine.textContent = `${Object.headline}`;
+    author.textContent = `${Object.authorName}`;
+    img.src = `${Object.authorPhoto}`;
+
+    card.appendChild(headLine);
+    card.appendChild(author);
+    author.appendChild(imgCont);
+    author.appendChild(name);
+    imgCont.appendChild(img);
+
+    return card;
+
+
+}
+
+const entryPoint = document.querySelector('.cards-container')
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then((response) => {
+        console.log(response);
+        response.data.articles.bootstrap.forEach((Object) => {
+            const newCard = card(Object);
+            entryPoint.appendChild(newCard);
+        });
+    })
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then((response) => {
+        response.data.articles.javascript.forEach((Object) => {
+            newCard = card(Object);
+            entryPoint.appendChild(newCard);
+        });
+    })
+
+axios.get('https://lambda-times-backend.herokuapp.com.articles')
+    .then((response) => {
+        response.data.articles.jquery.forEach((Object) => {
+            newCard = card(Object);
+            entryPoint.appendChild(newCard);
+        });
+    })
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then((response) => {
+        response.data.articles.node.forEach((Object) => {
+            newCard = card(Object);
+            entryPoint.appendChild(newCard);
+        });
+    })
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+    .then((response) => {
+        response.data.articles.technology.forEach((Object) => {
+            newCard = card(Object);
+            entryPoint.appendChild(newCard);
+        });
+    })
+
+    .catch((err) => {
+        console.log('error...no data available')
+    })
