754058de4c1fa8ec0fa27e72983479c730da8573
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-06-14 20:09:56 -0400

    adding topics.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,33 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+function tabs(arrItem) {
+    const topic = document.createElement('div');
+    topic.classList.add('tab');
+    topic.textContent - `${arrItem}`
+
+    return topic;
+
+}
+
+const insert = document.querySelector('.topics');
+console.log(insert);
+ 
+axios.get('https://lambda-times-backend.herokuapp.com/topics')
+    .then((response) => {
+        console.log(response)
+        response.data.topics.forEach((arrItem) => {
+            const newTab = tabs(arrItem);
+            insert.appendChild(newTab);
+        })
+    })
+    
+
+    .catch((err) => {
+        console.log('No data - error', err);
+            
+    });
+
+const topicTab = document.querySelector('.tab');
+topicTab.appendChild(tabs());
